e816c9cfa8a8436bc6d4bbbb01087d4dcd7c844a
Programmazione Time <46420439+ProgrammazioneTime@users.noreply.github.com>
2021-05-26 22:53:22 +0100

    Add files via upload

---
+++ primo-parziale/prova-A/stubs/ESA_03022021_A_2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define N 10
+void codifica(char [], int, int);
+// altri prototipi se necessario
+
+
+int main(){
+	char a[]={'1','a','Z','d','9','*','z','z','c','a'};
+	int i;
+	int num;
+	printf("\n Inserisci un intero non negativo: ");
+	do{
+		scanf("%d",&num);
+	}while(num<0);
+	codifica(a,N,num);
+	printf("\n");
+	for(i=0;i<N;i++)
+		printf("%c ",a[i]);
+	
+	return 0;
+		
+}
+
+
+void codifica(char a[], int dim, int k){
+
+
+
+
+
+	
+}
---
+++ primo-parziale/prova-A/stubs/ESA_03022021_A_3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ ulteriori direttive di pre-processing
+*/
+
+int main(void) {
+/*
+dichiarazioni
+*/
+srand(1);
+/*
+inizializzazione Matrix:
+*/
+
+/*
+somma per righe
+*/
+    
+/*
+somma per colonne
+*/
+    
+/*
+stampe di Matrix, SumR e SumC
+*/
+
+    
+  return 0;
+}
